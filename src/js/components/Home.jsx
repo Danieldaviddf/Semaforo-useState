424229f5380bf13ae9697943389c561e58ef6f63
Danieldaviddf <danieldaviddf@hotmail.com>
2026-03-11 16:05:55 +0100

    Creacion del boton Usame

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -10,33 +10,45 @@ const Home = () => {
 	const [amarillo, setAmarillo] = useState("btn btn-secondary");
 	const [rojo, setRojo] = useState("btn btn-secondary");
 
-	const cambioColor = () =>{
-		const nuevoVerde = verde === "btn btn-success" ? "btn btn-secondary" : "btn btn-success";
-    	setVerde(nuevoVerde); 
-		setAmarillo("btn btn-secondary");
-		setRojo("btn btn-secondary"); 
-	}
-	const cambioColor2 = () =>{
-		const nuevoAmarillo = amarillo === "btn btn-warning" ? "btn btn-secondary" : "btn btn-warning";
-    	setAmarillo(nuevoAmarillo); 
-		setVerde("btn btn-secondary");
-		setRojo("btn btn-secondary"); 
+		const cambioColor = () =>{
+			const nuevoVerde = verde === "btn btn-success" ? "btn btn-secondary" : "btn btn-success";
+			setVerde(nuevoVerde); 
+			setAmarillo("btn btn-secondary");
+			setRojo("btn btn-secondary"); 
+		}
+		const cambioColor2 = () =>{
+			const nuevoAmarillo = amarillo === "btn btn-warning" ? "btn btn-secondary" : "btn btn-warning";
+			setAmarillo(nuevoAmarillo); 
+			setVerde("btn btn-secondary");
+			setRojo("btn btn-secondary"); 
 
-	}
-	const cambioColor3 = () =>{
-		const nuevoRojo = rojo === "btn btn-danger" ? "btn btn-secondary" : "btn btn-danger";
-    	setRojo(nuevoRojo); 
-		setVerde("btn btn-secondary"); 
-		setAmarillo("btn btn-secondary");
+		}
+		const cambioColor3 = () =>{
+			const nuevoRojo = rojo === "btn btn-danger" ? "btn btn-secondary" : "btn btn-danger";
+			setRojo(nuevoRojo); 
+			setVerde("btn btn-secondary"); 
+			setAmarillo("btn btn-secondary");
+		}
+
+		const usame = () =>{
+			if(verde === "btn btn-success"){
+				cambioColor2();
+			} else if (amarillo === "btn btn-warning") {
+				cambioColor3();
+			} else {
+				cambioColor();
+			}
 	}
 
+	
 	return (
 		<div className="text-center">
-            <div className="btn-group-vertical" role="group" aria-label="Vertical button group">
-				<button type="button" className={verde} onClick={cambioColor}>Button</button>
-				<button type="button" className={amarillo} onClick={cambioColor2}>Button</button>
-				<button type="button" className={rojo} onClick={cambioColor3}>Button</button>
+            <div style={{ background: "#333", height: "600px", width: "220px", padding: "10px", borderRadius: "20px", margin: "0 auto" }} className="btn-group-vertical" role="group" aria-label="Vertical button group">
+				<button style={{ width: "200px", height: "200px", borderRadius: "50%"}} type="button" className={verde} onClick={cambioColor}>o</button>
+				<button style={{ width: "200px", height: "200px", borderRadius: "50%"}} type="button" className={amarillo} onClick={cambioColor2}>o</button>
+				<button style={{ width: "200px", height: "200px", borderRadius: "50%"}} type="button" className={rojo} onClick={cambioColor3}>o</button>
 			</div>
+			<button type="button" className="btn btn-primary" onClick={usame}>Usame</button>
 		</div>
 	);
 };
